680ceb6ce6e9992cfacbd85984557f52461fcdd4
Shahnewaz Rifat <rifat_rifats@live.com>
2023-02-03 05:36:00 +0600

    fix icons

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 rm -rf /usr/share/nginx/html/* && \
+aws s3 cp s3://tagbot-prod/AWS_Icon_Svg.zip /app/AWS_Icon_Svg.zip && unzip /app/AWS_Icon_Svg.zip -d /usr/share/nginx/html/ && \
 npm run build && \
 cp -R /app/dist/* /usr/share/nginx/html/ && \
 nginx -g "daemon off;"
