0a127d28017d90488c0bba575bb9726fa1f1709b
Danylo Zhytnyk <zhytnykdanylo@gmail.com>
2023-03-20 15:13:10 +0100

    fix: password button on the Login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,7 +9,10 @@
           </a>
         </div>
         <div class="burger-menu">
-          <div class="bar"><span class="bar-1"></span><span class="bar-2"></span><span class="bar-3"></span></div>
+          <div class="bar">
+            <span class="bar-1"></span><span class="bar-2"></span
+            ><span class="bar-3"></span>
+          </div>
         </div>
         <div class="right-header">
           <div></div>
@@ -19,43 +22,75 @@
     <main>
       <section class="main-section">
         <div class="content flex-main">
-          <div><h1 class="title">{{ $t("site.slogan") }}</h1>
+          <div>
+            <h1 class="title">{{ $t('site.slogan') }}</h1>
             <div class="main-form-block">
-              <div class="flex-welcome"><span class="weclome-to">{{ $t("site.welcome") }}</span><a href="#"
-                                                                                                   class="logotype">
-                <div class="tag-logo">Tag</div>
-                <div class="bot-logo">Bot</div>
-              </a></div>
-              <p class="text-form">{{ $t("site.tagline") }}</p>
+              <div class="flex-welcome">
+                <span class="weclome-to">{{ $t('site.welcome') }}</span
+                ><a href="#" class="logotype">
+                  <div class="tag-logo">Tag</div>
+                  <div class="bot-logo">Bot</div>
+                </a>
+              </div>
+              <p class="text-form">{{ $t('site.tagline') }}</p>
               <form autocomplete="off" class="main-form">
-                <div><input type="text" class="input-form" name="email" placeholder="Your email" value=""
-                            autocomplete=""></div>
+                <div>
+                  <input
+                    type="text"
+                    class="input-form"
+                    name="email"
+                    placeholder="Your email"
+                    value=""
+                    autocomplete=""
+                  />
+                </div>
+
                 <div class="block-pswrd">
-                  <div class="placeholder-container"><input type="password" name="password" class="focus-input"
-                                                            placeholder=" "
-                                                            value="" autocomplete=""><label>{{
-                      $t("common.password")
-                    }}</label><a
-                      href="#"
-                      class="show-password"><img
-                      src="/img/eye.svg" alt=""></a>
+                  <div class="placeholder-container">
+                    <input
+                      name="password"
+                      :type="isPasswordVisible ? 'text' : 'password'"
+                      class="focus-input"
+                      placeholder=" "
+                      value=""
+                      autocomplete=""
+                    />
+                      <label>{{ $t('common.password') }}</label>
+                      <div tabindex="0" @keyup.enter="toggleVisibilityOfPassword" @click="toggleVisibilityOfPassword" :class="['show-password', isPasswordVisible && 'show-password_visible']">
+                        <img src="/img/eye.svg" alt="" />
+                      </div>
                   </div>
                 </div>
                 <div class="flex-forgot">
-                  <div><input type="checkbox" class="checkbox"></div>
-                  <a href="#" class="forgot-password">{{ $t("common.forgot") }}</a></div>
-                <div><input type="submit" name="submit" class="form-submit modal-toggle" value="Sign In"></div>
+                  <div><input type="checkbox" class="checkbox" /></div>
+                  <a href="#" class="forgot-password">{{
+                    $t('common.forgot')
+                  }}</a>
+                </div>
+                <div>
+                  <input
+                    type="submit"
+                    name="submit"
+                    class="form-submit modal-toggle"
+                    value="Sign In"
+                  />
+                </div>
               </form>
               <div class="signin-block">
-                <p class="text-form">{{ $t("common.alt_signin") }}</p>
-                <el-button @click.prevent="loginWithGoogle()" class="signin-with" :loading="loading">
-                  <img src="/img/google-icon.svg" class="google-icon" alt="">
-                  <span class="google-text">Google</span></el-button>
+                <p class="text-form">{{ $t('common.alt_signin') }}</p>
+                <el-button
+                  @click.prevent="loginWithGoogle()"
+                  class="signin-with"
+                  :loading="loading"
+                >
+                  <img src="/img/google-icon.svg" class="google-icon" alt="" />
+                  <span class="google-text">Google</span></el-button
+                >
               </div>
               <div class="have-acc">
-                <span>{{ $t("common.no_account") }}</span>
+                <span>{{ $t('common.no_account') }}</span>
                 <router-link class="blue" to="/sign-up">
-                  {{ $t("common.signup") }}
+                  {{ $t('common.signup') }}
                 </router-link>
               </div>
             </div>
@@ -67,85 +102,100 @@
 </template>
 
 <script>
-import authService from "../services/auth"
-import {googleTokenLogin} from "vue3-google-login"
-import {userStore} from "../store/userStore";
+import authService from '../services/auth'
+import { googleTokenLogin } from 'vue3-google-login'
+import { userStore } from '../store/userStore'
 
 export default {
-  data() {
+  data () {
     return {
       email: '',
       password: '',
       remember_me: '',
-      loading: false
+      loading: false,
+      isPasswordVisible: false
     }
   },
   methods: {
-    loginWithGoogle() {
+    loginWithGoogle () {
       var self = this
       self.loading = true
-      googleTokenLogin().then((response) => {
+      googleTokenLogin().then(response => {
         // console.log("Handle the response", response)
         self.verifyGoogleLogin(response)
       })
     },
-    verifyGoogleLogin(response) {
+    verifyGoogleLogin (response) {
       var self = this
       var u = userStore()
-      this.$api.post('login/google', {
-        token: response.access_token
-      }).then((response) => {
-        // console.log(response)
-        authService.login(self.$pinia, response, this.remember_me)
-        self.loadProfile(u)
-        self.loading = false
-        self.$goTo('dashboard')
-      }).catch(function (error) {
-        // console.log(error)
-        self.loading = false
-        self.$toast.error(error.response.data)
-      })
+      this.$api
+        .post('login/google', {
+          token: response.access_token
+        })
+        .then(response => {
+          // console.log(response)
+          authService.login(self.$pinia, response, this.remember_me)
+          self.loadProfile(u)
+          self.loading = false
+          self.$goTo('dashboard')
+        })
+        .catch(function (error) {
+          // console.log(error)
+          self.loading = false
+          self.$toast.error(error.response.data)
+        })
     },
-    login() {
+    login () {
       var self = this
       self.loading = true
-      this.$api.post('auth/token', {
-        'email': this.email,
-        password: this.password,
-        remember_me: this.remember_me
-      }).then((response) => {
-        // console.log(response)
-        authService.login(this.$pinia, response, this.remember_me)
-        // toast.success("Login successful.")
-        self.loadProfile()
-        self.loading = false
-        self.$goTo('dashboard')
-      }).catch(function (error) {
-        // console.log(error)
-        self.loading = false
-        self.$toast.error(error.response.data)
-      })
+      this.$api
+        .post('auth/token', {
+          email: this.email,
+          password: this.password,
+          remember_me: this.remember_me
+        })
+        .then(response => {
+          // console.log(response)
+          authService.login(this.$pinia, response, this.remember_me)
+          // toast.success("Login successful.")
+          self.loadProfile()
+          self.loading = false
+          self.$goTo('dashboard')
+        })
+        .catch(function (error) {
+          // console.log(error)
+          self.loading = false
+          self.$toast.error(error.response.data)
+        })
     },
-    loadProfile(ustore) {
+    loadProfile (ustore) {
       var self = this
       var u = ustore
-      this.$api.get('profile').then((response) => {
-        u.setUser(response.data)
-        self.loadAccounts(u)
-        self.$mitt.emit('profile-loaded', {})
-      }).catch((error) => {
-        // console.log(error)
-        self.$toast.error(error.response.data)
-      })
+      this.$api
+        .get('profile')
+        .then(response => {
+          u.setUser(response.data)
+          self.loadAccounts(u)
+          self.$mitt.emit('profile-loaded', {})
+        })
+        .catch(error => {
+          // console.log(error)
+          self.$toast.error(error.response.data)
+        })
     },
-    loadAccounts(ustore) {
+    loadAccounts (ustore) {
       var u = ustore
-      console.log("Loading accounts now...")
-      this.$api.get('users/' + ustore.getData().id + '/accounts').then((response) => {
-        u.setAccounts(response.data)
-      }).catch((error) => {
-      })
+      console.log('Loading accounts now...')
+      this.$api
+        .get('users/' + ustore.getData().id + '/accounts')
+        .then(response => {
+          u.setAccounts(response.data)
+        })
+        .catch(error => {})
     },
+    toggleVisibilityOfPassword() {
+      this.isPasswordVisible = !this.isPasswordVisible
+    }
   }
 }
 </script>
@@ -156,8 +206,8 @@ export default {
   font-weight: 700;
   font-size: 16px;
   line-height: 20px;
-  color: #FFFFFF;
-  background: #4876EB !important;
+  color: #ffffff;
+  background: #4876eb !important;
   border: none;
   width: 100%;
   margin-top: 40px;
